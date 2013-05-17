f0594c71fa35edddee86fcbe4e49f937032fdc35
Dave Reid <dave@davereid.net>
2013-05-17 00:54:10 -0500

    Adding my modifications.

--- slide_config.js
+++ slide_config.js
@@ -12,22 +12,25 @@ var SLIDE_CONFIG = {
     enableSlideAreas: true, // Default: true. False turns off the click areas on either slide of the slides.
     enableTouch: true, // Default: true. If touch support should enabled. Note: the device must support touch.
     //analytics: 'UA-XXXXXXXX-1', // TODO: Using this breaks GA for some reason (probably requirejs). Update your tracking code in template.html instead.
-    favIcon: 'images/google_developers_logo_tiny.png',
+    favIcon: 'images/palantir/palantir-dot.png',
     fonts: [
       'Open Sans:regular,semibold,italic,italicsemibold',
       'Source Code Pro'
     ],
+    //sessionURL: 'http://www.example.com/my-slides',
+    //sessionShortURL: 'http://short.url/my-slides',
+    //timeLimit: '45:00',
     //theme: ['mytheme'], // Add your own custom themes or styles in /theme/css. Leave off the .css extension.
   },
 
   // Author information
   presenters: [{
-    name: 'Firstname Lastname',
-    company: 'Job Title<br>Google',
-    gplus: 'http://plus.google.com/1234567890',
-    twitter: '@yourhandle',
-    www: 'http://www.you.com',
-    github: 'http://github.com/you'
+    name: 'Dave Reid',
+    company: 'Senior Engineer<br>Palantir.net',
+    //gplus: 'http://plus.google.com/1234567890',
+    twitter: '@davereid',
+    www: 'http://www.daveried.net'
+    //github: 'http://github.com/davereid'
   }/*, {
     name: 'Second Name',
     company: 'Job Title, Google',
